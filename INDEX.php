15056553f2285983de986ff331c0445baa43bddc
princekoumi <leprincedan02@gmail.com>
2024-07-26 10:56:05 +0100

    first branch

---
+++ INDEX.php
@@ -0,0 +1,6 @@
+<?php
+   $name = "Bro code";
+   $food = "pizza";
+   echo"Hello {$name}<br>";
+   echo"You like {$food}";
+   ?
---
+++ index1.js
@@ -0,0 +1,3 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";let iceCream = "chocolate";
+let color = document.body.style.backgroundColor="grey";
